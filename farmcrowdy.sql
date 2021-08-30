2c7513c93d483435e74df152d590db5f8472cdbb
themordhon <romdhonm12@gmail.com>
2021-08-30 13:09:03 +0700

    backend fix

--- farmcrowdy.sql
+++ farmcrowdy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2021 at 04:41 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Aug 30, 2021 at 08:02 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -54,7 +54,7 @@ INSERT INTO `campaigns` (`id`, `user_id`, `name`, `short_description`, `descript
 (44, 32, 'Budidaya Aneka Sayuran Bogor - 3', 'Sayuran merupakan sumber vitamin dan mineral terutama karoten, Vit A, Vit C, zat besi dan fosfor', 'Sayuran merupakan sumber vitamin dan mineral terutama karoten, Vit A, Vit C, zat besi dan fosfor. Sebagian vitamin, mineral yang terkandung dalam sayuran berperan sebagai antioksidan. Beberapa sayuran dapat dikonsumsi mentah tanpa dimasak terlebih dahulu sementara yang lainnya dapat dimasak dengan cara dikukus, direbus dan ditumis.a', 'Sayuran merupakan komoditas hortikultura, selain memiliki nilai ekonomi yang cukup besar, ternyata juga memiliki potensi yang tinggi dalam pemenuhan gizi di dalam tubuh. Tanaman sayur yang mengandung berbagai zat gizi ini tidak hanya dapat digunakan sebagai konsumsi pangan sehari-hari, tetapi juga bagi pengobatan tradisional (herbal healing) dengan bahan-bahan alami. Seiring meningkatnya kesejahteraan, masyarakat semakin peduli terhadap faktor kesehatan, yakni dengan mengkonsumsi makanan sehat dan bermutu maka permintaan sayuran pun menjadi lebih tinggi sehingga pasarnya luas.b', 'Cuaca ekstrim yang sering berubah – ubah seperti sering turun hujan yang sangat lebat akan menyebabkan daerah penanaman terserang banjir, selain itu jika terjadi cuaca yang sangat panas maka akan menyebabkan kekeringan yang sangat parah.c', 'Kelompok Tani DH merupakan kelompok tani yang berada di daerah Jawa Barat. Kelompok tani ini sudah berdiri sejak tahun 2016 dengan 5 orang anggota kelompok. Kelompok tani ini sudah berpengalaman dalam melakukan budidaya aneka sayuran selama 5 tahun. Berdasarkan pengalaman kelompok tani ini maka TaniFund tertarik untuk melakukan kerja sama.d', 897000000, 597000000, 'Saham 10% dari hasil panen,menjadi mitra', 'budidaya-aneka-sayuran-bogor-2-32', '3', '2021-08-20 20:19:53.269928', '2021-08-21 21:09:20.786875'),
 (45, 52, 'Budidaya Ikan Nila Subang - 3 ', 'Ikan air tawar yang banyak dikonsumsi masyarakat Indonesia.', 'Ikan nila dengan nama Latin Oreochromis niloticus merupakan ikan air tawar yang banyak dikonsumsi masyarakat Indonesia. Ikan nila mempunyai tubuh memanjang dan ramping, sisik ikan nila relatif besar, matanya menonjol dan besar dengan tepi berwarna putih. Ikan nila mempunyai lima buah sirip yang berada di punggung, dada, perut, anus, dan ekor. Ikan nila toleran terhadap perbedaan lingkungan sangat tinggi, dapat hidup pada salinitas 0-29 permil; suhu 14-38 °C; pH 5 — 11.', 'Ikan nila dikenal dengan pertumbuhannya yang sangat cepat, yakni hanya membutuhkan waktu 4 hingga 6 bulan untuk pembesarannya hingga memenuhi ukuran standar untuk dikonsumsi. Ikan nila dianggap cukup kuat terhadap gangguan penyakit yang biasa dialami oleh jenis ikan lainnya, sehingga perawatannya mudah dan biayanya murah, namun hasilnya relatif menguntungkan.', 'Bobot ikan yang tidak ideal dikarenakan ada kemungkinan pakan yang diberikan tidak mengandung protein yang cukup. Maka dari itu Dalam pemberian pakan, harus diperhitungkan juga sample saat menentukan berat badan ikan, yaitu sebagai patokan ketika pakan diberikan. ', 'MN 37 adalah pelaku budidaya yang memiliki expertise dan pengetahuan mendalam di bidang perikanan yang berlokasi di wilayah Subang, Jawa Barat. Kelompok ternak yang diketuai oleh Bapak Jaja Subagja beranggotakan 4 orang. Berdiri sejak 3 tahun yang lalu kelompok ternak ini memiliki lahan sekiar 1 Ha. Didukung oleh kinerja dan pengalaman budidaya para peternak yang baik mendasari Tanifund untuk bisa membantu memberikan pendanaan kepada kelompok ternak tersebut.', 412400000, 412400000, 'saham penghasilan 10%, menjadi mitra tambang ikan kami', 'budidaya-ikan-nila-subang-3-52', '2', '2021-08-21 14:28:26.392247', '2021-08-21 14:28:26.392247'),
 (46, 52, 'Budidaya Jamur Tiram Kuningan 1', 'Jamur tiram merupakan bahan makanan bernutrisi yang kaya akan vitamin dan mineral, rendah karbohidrat, lemak, dan protein.', 'Jamur tiram atau dengan nama latin Pleurotus ostreatus merupakan salah satu kelompok jamur yang sudah banyak dikenal karena bentuk dan ukuran tubuh buahnya yang sangat familiar di masyarakat. Ciri-ciri umum tubuh buah berwarna putih hingga krem dan tudungnya berbentuk setengah lingkaran mirip cangkang yang bagian tengahnya agak cekung. Jamur tiram sering dikenal dengan sebutan King Oyster Mushroom dan merupakan salah satu jenis jamur yang dapat dimakan (edible) serta memiliki rasa yang cukup khas. Jamur tiram tumbuh optimal pada kayu lapuk yang tersebar di dataran rendah sampai lereng pegunungan atau kawasan yang memiliki ketinggian antara 600 m-800 m diatas permukaan laut. Miselium dan tubuh buahnya tumbuh dan berkembang baik pada suhu yang lembab berkisar pada 26-30 °C.', 'Jamur tiram (Pleurotus ostreatus) merupakan bahan makanan bernutrisi yang kaya akan vitamin dan mineral, rendah karbohidrat, lemak, dan protein. Jamur tiram juga memiliki berbagai manfaat yaitu sebagai makanan, menurunkan kolesterol, sebagai anti bakterial dan anti tumor. Dilihat dari kandungan gizi yang terdapat dalam jamur tiram maka bahan ini termasuk aman untuk dikonsumsi.', 'Serangan serangga atau lalat terhadap jamur sangat besar. Permasalahan hama penyakit dapat teratasi dengan menjaga kebersihan tempat, penggunaan pestisida atau feromon (cairan hormon perangkap hama). Selain itu, tempat tumbuh jamur selalu terjaga kelembabannya. Pasalnya, untuk menunjang tumbuhnya badan jamur, dibutuhkan kelembaban yang tinggi.', 'Kelompok Tani AM merupakan kelompok tani yang berada di daerah Jawa Barat. Kelompok tani ini sudah berdiri sejak tahun 2017 dengan 5 orang anggota kelompok. Kelompok tani yang sudah berpengalaman dalam melakukan budidaya jamur tiram ini melakukan budidaya di atas lahan seluas 0.2 Ha. Berdasarkan pengalaman kelompok tani ini maka TaniFund tertarik untuk melakukan kerja sama.', 420700000, 0, 'Hasil panen 20%,Pemilik saham 10%', 'budidaya-jamur-tiram-kuningan-1-52', '0', '2021-08-21 20:00:16.478650', '2021-08-21 20:45:02.647898'),
-(47, 32, 'Budidaya Pembesaran Ikan Mas dan Ikan Nila Bandung Barat 1', 'kan mas merupakan ikan air tawar yang mempunyai nilai ekonomis. ', '', '', '', '', 174800000, 0, 'Hasil Keuntungan 10%, Min Kontribusi Rp 100k', 'budidaya-pembesaran-ikan-mas-dan-ikan-nila-bandung-barat-1-32', '0', '2021-08-21 20:15:45.878134', '2021-08-21 21:39:01.019697');
+(47, 32, 'Budidaya Pembesaran Ikan Mas dan Ikan Nila Bandung Barat 1', 'kan mas merupakan ikan air tawar yang mempunyai nilai ekonomis. ', 'Ikan nila dan ikan mas adalah salah satu jenis ikan yang paling terkenal di Indonesia. Ikan mas merupakan ikan air tawar yang mempunyai nilai ekonomis. Ikan mas memiliki badan berbentuk memanjang dan sedikit pipih ke samping dan mulutnya terletak di ujung tengah, memiliki dua pasang sungut serta warna badan yang sangat beragam. Sedangkan ikan nila mempunyai tubuh memanjang dan ramping, sisik ikan nila relatif besar, matanya menonjol dan besar dengan tepi berwarna putih.', 'kan mas dan ikan nila layak dibudidayakan karena tingginya minat di pasar. Permintaan ini biasanya tidak datang dari rumah tangga saja tapi banyak dijadikan sebagai menu utama pada berbagai hidangan rumah makan. Keberadaan ikan nila dan ikan mas mampu menyuplai kebutuhan protein hewani masyarakat secara luas karena harganya yang relatif terjangkau. Jika dikelola oleh pelaku budidaya yang memiliki pengetahuan mendalam terkait nila, budidaya ikan nila menguntungkan. \r\n ', 'Bobot ikan yang tidak ideal dikarenakan ada kemungkinan pakan yang diberikan tidak mengandung protein yang cukup. Maka dari itu Dalam pemberian pakan, harus diperhitungkan juga sample saat menentukan berat badan ikan, yaitu sebagai patokan ketika pakan diberikan.\r\n ', 'Kelompok Ternak IKM merupakan kelompok ternak yang berada di daerah Jawa Barat. Kelompok ternak ini sudah berdiri sejak tahun 2016 dengan 5 orang anggota kelompok. Kelompok ternak ini sudah berpengalaman dalam melakukan budidaya ikan mas dan ikan nila selama 5 tahun. Berdasarkan pengalaman kelompok ternak ini maka TaniFund tertarik untuk melakukan kerja sama.', 174800000, 0, 'Hasil Keuntungan 10%,Min Kontribusi Rp 100k', 'budidaya-pembesaran-ikan-mas-dan-ikan-nila-bandung-barat-1-32', '0', '2021-08-21 20:15:45.878134', '2021-08-30 12:48:53.311360');
 
 -- --------------------------------------------------------
 
@@ -76,17 +76,17 @@ CREATE TABLE `campaign_images` (
 --
 
 INSERT INTO `campaign_images` (`id`, `campaign_id`, `file_name`, `is_primary`, `created_at`, `updated_at`) VALUES
-(60, 1, 'images/32-qamma-farm-L6T_mq0bwwA-unsplash.jpg', 0, '2021-08-18 17:54:01.900259', '2021-08-19 14:18:27.457156'),
 (61, 2, 'images/2-qamma-farm-L6T_mq0bwwA-unsplash.jpg', 1, '2021-08-18 18:01:54.544922', '2021-08-18 18:01:54.544922'),
 (62, 31, 'images/2-qamma-farm-L6T_mq0bwwA-unsplash.jpg', 1, '2021-08-18 18:02:05.299328', '2021-08-18 18:02:05.299328'),
 (63, 35, 'images/1-qamma-farm-UfGEUNbnh_8-unsplash.jpg', 1, '2021-08-18 18:02:15.855369', '2021-08-18 18:02:15.855369'),
 (64, 36, 'images/32-qamma-farm-DFSc_C9Recw-unsplash.jpg', 1, '2021-08-18 18:02:25.504446', '2021-08-18 18:02:25.504446'),
 (65, 37, 'images/32-qamma-farm-UfGEUNbnh_8-unsplash.jpg', 1, '2021-08-18 18:02:36.035929', '2021-08-18 18:02:36.035929'),
-(66, 1, 'images/32-qamma-farm-DFSc_C9Recw-unsplash.jpg', 1, '2021-08-19 14:18:27.605083', '2021-08-19 14:18:27.605083'),
 (67, 44, 'images/32-gambap buah sayur.webp', 1, '2021-08-20 20:21:07.356357', '2021-08-20 20:21:07.356357'),
 (68, 45, 'images/52-ikan.webp', 1, '2021-08-21 14:29:39.541996', '2021-08-21 14:29:39.541996'),
 (69, 46, 'images/52-jamur.jpg', 1, '2021-08-21 20:02:14.611548', '2021-08-21 20:02:14.611548'),
-(70, 47, 'images/32-ikan-mas.webp', 1, '2021-08-21 20:17:59.655128', '2021-08-21 20:17:59.655128');
+(70, 47, 'images/32-ikan-mas.webp', 1, '2021-08-21 20:17:59.655128', '2021-08-21 20:17:59.655128'),
+(71, 1, 'images/32-porang.jpg', 0, '2021-08-30 12:43:50.714968', '2021-08-30 12:46:34.357879'),
+(72, 1, 'images/32-porang2.jpg', 1, '2021-08-30 12:46:34.395716', '2021-08-30 12:46:34.395716');
 
 -- --------------------------------------------------------
 
@@ -196,7 +196,7 @@ ALTER TABLE `campaigns`
 -- AUTO_INCREMENT for table `campaign_images`
 --
 ALTER TABLE `campaign_images`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT for table `transactions`
